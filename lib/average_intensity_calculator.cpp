b0badfb1e3c8ebbab83f4e15a4561328d8ea89e5
char-lie <5962274+char-lie@users.noreply.github.com>
2022-01-01 13:17:13 +0200

    Refactor the project
    - Apply `clang-format`
    - Add license to each file

--- lib/average_intensity_calculator.cpp
+++ lib/average_intensity_calculator.cpp
@@ -1,14 +1,34 @@
+// MIT License
+//
+// Copyright (c) 2021-2022 char-lie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "average_intensity_calculator.hpp"
 
 #include <numeric>
 #include <opencv2/imgproc.hpp>
 #include <vector>
 
-void AverageIntensityCalculator::replace_image(const cv::Mat& image)
-{
+void AverageIntensityCalculator::replace_image(const cv::Mat& image) {
   cv::cvtColor(image, this->images[0], cv::COLOR_RGB2GRAY);
-  if (this->images[0].type() != CV_8U or this->images[0].channels() != 1)
-  {
+  if (this->images[0].type() != CV_8U or this->images[0].channels() != 1) {
     throw std::runtime_error{
         "Something is wrong with `cv::cvtColor` function: `cv::COLOR_RGB2GRAY` "
         "conversion is expected to create a grayscale image with [0; 255] "
--- lib/thread_handle.cpp
+++ lib/thread_handle.cpp
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2021-2022 char-lie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "thread_handle.hpp"
 
 #include <concepts>
@@ -11,16 +33,14 @@ bool ThreadHandle::need_stop() {
   std::unique_lock<std::mutex> lock{this->mutex};
 
   while (this->waiting and !this->is_finished) {
-    this->condition_variable.wait(lock, [this] {
-      return !this->waiting or this->is_finished;
-    });
+    this->condition_variable.wait(
+        lock, [this] { return !this->waiting or this->is_finished; });
   }
 
   return this->is_finished;
 }
 
-bool ThreadHandle::try_show_image()
-{
+bool ThreadHandle::try_show_image() {
   std::lock_guard queue_image_guard{this->image_mutex};
   if (this->output_image.has_value()) {
     cv::imshow(this->window_name, *this->output_image);
@@ -30,9 +50,8 @@ bool ThreadHandle::try_show_image()
   return false;
 }
 
-void ThreadHandle::finish()
-{
-    std::lock_guard<std::mutex> lock{this->mutex};
-    this->is_finished = true;
-    this->condition_variable.notify_one();
+void ThreadHandle::finish() {
+  std::lock_guard<std::mutex> lock{this->mutex};
+  this->is_finished = true;
+  this->condition_variable.notify_one();
 }
